fdd32456cb3904e1a39befd5165eaa59b8ff2b69
jackielfu <jackielfu@gmail.com>
2018-01-03 15:32:25 -0600

    Add request handler file

--- server/index.js
+++ server/index.js
@@ -1,19 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
-// var items = require('../database-mysql');
-// var items = require('../database-mongo');
+var items = require('../database-mysql');
+var requestHandler = require('./request-handler.js');
 
 var app = express();
 
-// UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../react-client/dist'));
 
-// UNCOMMENT FOR ANGULAR
-// app.use(express.static(__dirname + '/../angular-client'));
-// app.use(express.static(__dirname + '/../node_modules'));
-
-app.get('/items', function (req, res) {
+app.get('/', function (req, res) {
   items.selectAll(function(err, data) {
     if(err) {
       res.sendStatus(500);
