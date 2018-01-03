cc147625fcf835484ec2f376da395ff22d63a9de
jackielfu <jackielfu@gmail.com>
2018-01-03 17:14:00 -0600

    Create server template with get and post requests

--- server/index.js
+++ server/index.js
@@ -6,16 +6,10 @@ var requestHandler = require('./request-handler.js');
 var app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
-
-app.get('/', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,99 +1,50 @@
-var results = [];
+var express = require('express');
+var bodyParser = require('body-parser');
+var database = require('../database-mysql/index.js');
 
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-  //console.log(request);
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-  // The outgoing status.
-  
-  var defaultCorsHeaders = {
-    'access-control-allow-origin': '*',
-    'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    'access-control-allow-headers': 'content-type, accept',
-    'access-control-max-age': 10 // Seconds.
-  };
-  
-  var headers = defaultCorsHeaders;
-  headers['Content-Type'] = 'application/json';
-  
-  var statusCode = 200;
-  var stringifiedResponse;
-  
-  if (request.method === 'POST' && (request.url === '/classes/messages' || request.url === '/classes/rooms')) {
-    statusCode = 201;
-    var body = '';
-    request.on('data', function(chunk) {
-      body += chunk;
-    });
+var app = express();
 
-    request.on('end', function() {
-      results.push(JSON.parse(body));
-      response.writeHead(201, headers);
-      response.end(JSON.stringify(body));
-    });
-    
-  } else if (request.method === 'GET' && (request.url === '/classes/messages' || request.url === '/classes/rooms')) {
-    statusCode = 200;
-    stringifiedResponse = JSON.stringify({results: results});
-    response.writeHead(200, headers);
-    response.end(stringifiedResponse);
-    
-  } else if (request.method === 'OPTIONS' && (request.url === '/classes/messages' || request.url === '/classes/rooms')) {
-    statusCode = 200;
-    response.writeHead(200, headers);
-    response.end(stringifiedResponse);
-    
-  } else {
-    // statusCode = 404;
-    response.writeHead(404, headers);
-    response.end();
-  }
+/***** Handle GET requests ******/
+app.get('/', function (req, res) {
+  // Serves up index.html (currently login page)
+  // Will refactor to redirect to user profile
+  // once we handle cookies
+  if (err) { return res.sendStatus(400); } 
+  else { res.json(); }
+});
 
-  // These headers will allow Cross-Origin Resource Sharing (CORS).
-  // This code allows this server to talk to websites that
-  // are on different domains, for instance, your chat client.
-  //
-  // Your chat client is running from a url like file://your/chat/client/index.html,
-  // which is considered a different domain.
-  //
-  // Another way to get around this restriction is to serve you chat
-  // client from this domain by setting up static file serving.
-  
-  
-  
+app.get('/login', function(req, res) {
+  // Renders login page
+  if (err) { return res.sendStatus(400); } 
+  else { res.json(); }
+});
 
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
+app.get('/goals', function(req, res) {
+  // Will fetch goals for the specific user
+  if (err) { return res.sendStatus(400); } 
+  else { res.json(); }
+});
 
-  
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  
 
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
+/***** Handle POST requests ******/
+app.post('/login', function(req, res) {
+  // Will cross reference login credentials
+  // with db to confirm or deny login
+  if (err) { return res.sendStatus(400); } 
+  else { res.json(); }
+});
 
-};
+app.post('/signup', function(req, res) {
+  // Will add user to db, making sure
+  // they are not using a taken username
+  if (err) { return res.sendStatus(400); } 
+  else { res.json(); }
+});
 
+app.post('/goals', function(req, res) {
+  // Will add goals to user in database
+  if (err) { return res.sendStatus(400); } 
+  else { res.json(); }
+});
 
 module.exports.requestHandler = requestHandler;
