c0ad4edaca84d13bee32657f6c16d41c6aeaa7eb
Jongerr <jontmichie@gmail.com>
2018-01-03 16:25:00 -0600

    Add user_id foreign key to database schema

--- database-mysql/schema.sql
+++ database-mysql/schema.sql
@@ -9,7 +9,12 @@ CREATE TABLE goals (
   description varchar(200)  NOT NULL,
   punishment varchar(200) NOT NULL,
   frequency int NOT NULL,
-  PRIMARY KEY (ID)
+  user_id INT,
+    INDEX us_ind (user_id),
+    FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+  PRIMARY KEY (id)
 );
 
 
@@ -18,7 +23,7 @@ CREATE TABLE users (
   id int NOT NULL AUTO_INCREMENT,
   username varchar(40) NOT NULL,
   password varchar(50) NOT NULL,
-  PRIMARY KEY (ID)
+  PRIMARY KEY (id)
 );
 
 
