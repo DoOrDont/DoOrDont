8d2bb6d93442daab7933fcc434a4c8478de1efd4
Jongerr <jontmichie@gmail.com>
2018-01-03 17:19:56 -0600

    Change db schema and helper query string.

--- database-mysql/helpers/models.js
+++ database-mysql/helpers/models.js
@@ -1,10 +1,11 @@
 const connection = require('../index.js').connection;
 
 const getGoalsForUser = (username, callback) => {
-  connection.query('SELECT * FROM items', function (err, results, fields) {
+  connection.query('SELECT * FROM goals INNER JOIN users ON goals.user_id=(SELECT id FROM users WHERE username=' + username + ')', function (err, results, fields) {
     if (err) {
       callback(err, null);
     } else {
+      console.log('DB results:', results);
       callback(null, results);
     }
   });
