cb7a276feab1e90a90cd31333077625bae6ee912
Jongerr <jontmichie@gmail.com>
2018-01-13 11:02:49 -0600

    Setup basic twitter messaging

--- server/auth/twitter.js
+++ server/auth/twitter.js
@@ -1,44 +1,62 @@
-var express = require('express');
-var passport = require('passport-twitter');
-var Strategy = require('passport-twitter').Strategy;
+const Twit = require('twit');
 
-var config = require('./config.js');
+const TWIT_CONFIG = {
+  consumer_key: process.env.TWIT_CONSUMER_KEY,
+  consumer_secret: process.env.TWIT_CONSUMER_SECRET,
+  access_token: process.env.TWIT_ACCESS_TOKEN,
+  access_token_secret: process.env.TWIT_TOKEN_SECRET,
+  timeout_ms: 60 * 1000
+};
+
+console.log(TWIT_CONFIG);
+
+const T = new Twit(TWIT_CONFIG);
+
+// T.post('statuses/update', { status: 'Test status again' }, function (err, data, response) {
+//   console.log(data)
+// })
+
+// var express = require('express');
+// var passport = require('passport-twitter');
+// var Strategy = require('passport-twitter').Strategy;
+
+// var config = require('./config.js');
 
 
-passport.use(new TwitterStrategy({
-    consumerKey: config.TWITTER_CONSUMER_KEY,
-    consumerSecret: config.TWITTER_CONSUMER_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
-  },
-  function(token, tokenSecret, profile, cb) {
-    // send token to db to be stored with the corresponding user in the db
-    db.addTwitterCreds();
-    // User.findOrCreate({ twitterId: profile.id }, function (err, user) {
-      // return cb(err, user);
-    // });
-  }
-));
+// passport.use(new TwitterStrategy({
+//     consumerKey: config.TWITTER_CONSUMER_KEY,
+//     consumerSecret: config.TWITTER_CONSUMER_SECRET,
+//     callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+//   },
+//   function(token, tokenSecret, profile, cb) {
+//     // send token to db to be stored with the corresponding user in the db
+//     db.addTwitterCreds();
+//     // User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+//       // return cb(err, user);
+//     // });
+//   }
+// ));
 
-// Will supply the user ID when serializing 
-module.exports.serialize = passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
+// // Will supply the user ID when serializing 
+// module.exports.serialize = passport.serializeUser(function(user, done) {
+//   done(null, user.id);
+// });
 
-// Will query the user record by ID from the database when deserializing
-module.exports.deserialize = passport.deserializeUser(function(id, done) {
-  connection.query("SELECT * FROM users WHERE id = ?", [id], function(err, rows){ 
-    done(err, rows[0]);
-  });
-});
+// // Will query the user record by ID from the database when deserializing
+// module.exports.deserialize = passport.deserializeUser(function(id, done) {
+//   connection.query("SELECT * FROM users WHERE id = ?", [id], function(err, rows){ 
+//     done(err, rows[0]);
+//   });
+// });
 
-/************ ROUTING **************/
-app.get('/auth/twitter', 
-         passport.authenticate('twitter'),
-         // optional handler
-         function(req, res) {}); 
-app.get('/auth/twitter/callback', 
-         passport.authenticate('twitter', { 
-                                successRedirect: '/',
-                                failureRedirect: '/login' }),
-         // optional handler
-         function(req, res) {});
+// /************ ROUTING **************/
+// app.get('/auth/twitter', 
+//          passport.authenticate('twitter'),
+//          // optional handler
+//          function(req, res) {}); 
+// app.get('/auth/twitter/callback', 
+//          passport.authenticate('twitter', { 
+//                                 successRedirect: '/',
+//                                 failureRedirect: '/login' }),
+//          // optional handler
+//          function(req, res) {});
