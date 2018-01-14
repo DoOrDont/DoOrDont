816b7fb84a47d5b548a61de992c328372105bb73
Jason McLaughlin <jasonstephenm@gmail.com>
2018-01-14 17:34:53 -0600

    Refactor inspirational quote functionality

--- react-client/src/components/Quote.jsx
+++ react-client/src/components/Quote.jsx
@@ -1,6 +1,39 @@
 import React from 'react';
 const axios = require('axios');
 
+const quotes = [
+  { text: 'The least movement is of importance to all nature. The entire ocean is affected by a pebble.', 
+    author: 'Blaise Pascal'
+  },
+  { text: 'The key to growth is the introduction of higher dimensions of consciousness into our awareness.', 
+    author: 'Lao Tzu'
+  },
+  { text: 'It does not matter how slowly you go as long as you do not stop.', 
+    author: 'Confucius'
+  },
+  { text: 'The first step to getting the things you want out of life is this: decide what you want.', 
+    author: 'Ben Stein'
+  },
+  { text: 'A good plan today is better than a perfect plan tomorrow.', 
+    author: 'unknown'
+  },
+  { text: 'Well done is better than well said.', 
+    author: 'Benjamin Franklin'
+  },
+  { text: 'Obstacles are those things you see when you take your eyes off the goal.', 
+    author: 'Hannah More'
+  },
+  { text: 'There are no failures. Just experiences and your reactions to them.', 
+    author: 'Tom Krause'
+  },
+  { text: 'The person who makes a success of living is the one who see his goal steadily and aims for it unswervingly. That is dedication.', 
+    author: 'Cecil B. DeMille'
+  },
+  { text: 'If you find yourself in a hole, the first thing to do is stop digging.', 
+    author: 'Will Rogers'
+  },
+]
+
 class Quote extends React.Component {
   constructor(props) {
     super(props);
@@ -8,27 +41,13 @@ class Quote extends React.Component {
       quote: 'do or do not',
       author: 'unknown'
     };
-    this.updateQuote = this.updateQuote.bind(this);
-  }
-
-  updateQuote(obj){
-    if(obj.data.contents.quotes[0].quote){
-      this.setState({
-        quote: obj.data.contents.quotes[0].quote,
-        author: obj.data.contents.quotes[0].author
-      });
-    }
   }
 
   componentDidMount() {
-    axios.get('http://quotes.rest/qod.json?category=inspire')
-    .then((response) => {
-      console.log(response.data.contents.quotes[0].author);
-      this.updateQuote(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    let randomNum = Math.floor(Math.random() * 10);
+    let quote = quotes[randomNum]['text'];
+    let author= quotes[randomNum]['author'];
+    this.setState({quote: quote, author: author});
   }
 
   render() {
